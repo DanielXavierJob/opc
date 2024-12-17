36a59401843bc26b3fc3c3ab8db9347a646f7f79
Daniel Xavier <danielxavier.job@gmail.com>
2024-12-17 13:34:19 -0300

    fix: removed hard code

--- connection_windows.go
+++ connection_windows.go
@@ -233,12 +233,7 @@ func (ao *AutomationObject) Close() error {
 }
 
 // NewAutomationObject connects to the COM object based on available wrappers.
-func NewAutomationObject(wrapperss []string) (*AutomationObject, error) {
-	wrappers := []string{
-		"OPC.Automation.1",
-		"Graybox.OPC.DAWrapper.1",
-		"Matrikon.OPC.Automation.1",
-	}
+func NewAutomationObject(wrappers []string) (*AutomationObject, error) {
 	var err error
 	var unknown *ole.IUnknown
 	for _, wrapper := range wrappers {
